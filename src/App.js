9b70ebbea4846da8df7947600d8b76decc3c6ce7
Marushini <marushinibabu2203@gmail.com>
2024-12-06 11:29:35 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Discussion from "./pages/Discussion";
 
 const App = () => {
