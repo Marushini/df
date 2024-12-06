3c4ef196c351b832c85da73c801408927edaf1d8
Marushini <marushinibabu2203@gmail.com>
2024-12-06 11:13:08 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import Header from './components/Header';
-import PostList from './components/PostList';
-import NewPost from './components/NewPost';
-import PostDetails from './components/PostDetails';
-
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<PostList />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/new-post" element={<NewPost />} />
-        <Route path="/post/:id" element={<PostDetails />} />
-      </Routes>
-    </Router>
+    <div className="container mt-5">
+      <h1>Welcome to the Discussion Forum</h1>
+    </div>
   );
 }
 
